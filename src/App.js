16897c82a0b8424c0ed6476c4a54abaab9608310
susan <odiisusan@gmail.com>
2023-02-10 13:07:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  return (
+    <div className="bg-slate">
+     <h1 className='text-xl'>jehovah </h1> 
+    <button className="btn btn-primary">submit</button>
+    <h1>hhh</h1>
+    <h2>jesus</h2>
+    </div>
+  );
+}
+
+export default App;
