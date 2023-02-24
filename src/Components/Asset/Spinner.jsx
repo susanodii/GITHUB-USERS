ad5c9a98874b87d41d41608f1a2626cadb611e85
susan <odiisusan@gmail.com>
2023-02-24 09:14:19 +0100

    reduced spinner width

--- src/Components/Asset/Spinner.jsx
+++ src/Components/Asset/Spinner.jsx
@@ -1,7 +1,6 @@
-
 import spinners from '../Asset/spinners.jpg'
 
- const Spinner = () => {
+const Spinner = () => {
   return (
     <div className='w-100 mt-20'>
 <img width={200} className='text-center mx-auto' src={spinners}
