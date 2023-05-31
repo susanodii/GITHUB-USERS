e27e9c2acaee381259aee08303f1605cc9b68d7b
susan <odiisusan@gmail.com>
2023-05-31 04:19:24 +0100

    added user details to view

--- src/Components/Pages/User.jsx
+++ src/Components/Pages/User.jsx
@@ -1,58 +1,136 @@
-import {FaCode, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
-import {Link, useParams} from 'react-router-dom'
-import {useContext, useEffect} from 'react'
+import { FaCode, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
+import { Link, useParams } from 'react-router-dom'
+import { useContext, useEffect } from 'react'
 
 import GithubContext from '../Context/Github/GithubContext'
 import Spinner from '../Asset/Spinner'
 
 const User = () => {
-  const {getUser, user, isLoading} = useContext(GithubContext)
+  const { getUser, user, isLoading } = useContext(GithubContext)
   const params = useParams()
-  console.log(user,  'initial users' )
- 
-  useEffect(()=>{
-getUser(params.login);
+  console.log(user, 'initial users')
 
+  useEffect(() => {
+    getUser(params.login)
   }, [])
 
-  if(isLoading){
-    return<Spinner/>
+  if (isLoading) {
+    return <Spinner />
   }
 
   // distructuture the user data from api
-  const {name, type, avatar_url, location, bio, blog, twitter_username, login, html_url, followers, following, public_repos, public_gists, hireable} = user
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user
 
   return (
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to="/" className="btn btn-ghost">
+            Back to Search Page
+          </Link>
+          <div className="grid grid-cols-1 xl:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8 ">
+            <div className="mb-6 md:mb-0 ">
+              <div className="rounded-lg shadow-xl card-image-full">
+                <figure>
+                  <img src={avatar_url} alt="user-profile pics" />
+                </figure>
+                <div className="card-body justify-end">
+                  <h2 className="card-title mt-40">{name}</h2>
+                  <p>{login}</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-2">
+              <div className="mb-6">
+                <h1 className="text-3xl card-title">
+                  {name}
+                  <div className="ml-2 mr-1 badge badge-success text-white">
+                    {type}
+                  </div>
+                  <div>
+                    {hireable && (
+                      <div className="mx-1 badge badge-info text-white">
+                        Hireable{' '}
+                      </div>
+                    )}
+                  </div>
+                </h1>
+                <p>{bio}</p>
+                <div className="mt-4 card-actions">
+                  <a
+                    href={html_url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn"
+                  >
+                    {' '}
+                    Visit Github Profile
+                  </a>
+                </div>
+              </div>
+              <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+                {location && (
+                  <>
+                    <div className="stat-title text-md"> Location</div>
+                    <div className="text-lg stat-value"> {location}</div>
+                  </>
+                )}
 
-    < >
-    <div className="w-full mx-auto lg:w-10/12">
+                {blog && (
+                  <>
+                    <div className="mt-2 stat-title text-md"> Website</div>
+                    <div className="text-lg stat-value">
+                      {' '}
+                      <a
+                        href={`https://${blog}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {' '}
+                        {blog}{' '}
+                      </a>
+                    </div>
+                  </>
+                )}
 
-      <div className='mb-4'>
-        <Link to='/' className='btn btn-ghost'>
-        Back to Search Page
-        </Link>
-        <div className='grid grid-cols-1 xl:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8 '>
-        <div className='custom-card-image mb-6'>
-<div className='rounded-lg shadow-xl card-image-full'>
-  <figure>
-    <img src={avatar_url} alt='user-profile pics'/>
-  </figure>
-<div className="card-body justify-end"></div>
-<h2 className='card-title mb-0'>
-{name}
-</h2>
-<p>
-  {login}
-</p>
-</div>
-</div>
+
+{twitter_username && (
+                  <>
+                    <div className="mt-2 stat-title text-md"> Twitter</div>
+                    <div className="text-lg stat-value">
+                      {' '}
+                      <a
+                        href={`https://twitter.com/${twitter_username}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {' '}
+                        {blog}{' '}
+                      </a>
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
-    </div>
-    
     </>
-    
   )
 }
 
