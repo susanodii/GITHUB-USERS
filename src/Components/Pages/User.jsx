b522b602921b9c254a74a98fa7bf385373b3a3f7
susan <odiisusan@gmail.com>
2023-05-31 10:45:31 +0100

    updated the design for user profile

--- src/Components/Pages/User.jsx
+++ src/Components/Pages/User.jsx
@@ -43,93 +43,150 @@ const User = () => {
           <Link to="/" className="btn btn-ghost">
             Back to Search Page
           </Link>
-          <div className="grid grid-cols-1 xl:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8 ">
-            <div className="mb-6 md:mb-0 ">
-              <div className="rounded-lg shadow-xl card-image-full">
-                <figure>
-                  <img src={avatar_url} alt="user-profile pics" />
-                </figure>
-                <div className="card-body justify-end">
-                  <h2 className="card-title mt-40">{name}</h2>
-                  <p>{login}</p>
-                </div>
+        </div>
+        <div className="grid grid-cols-1 xl:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8 ">
+          <div className="mb-6 md:mb-0 ">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="user profile pics" />
+              </figure>
+              <div className="card-body justify-end">
+                <h2 className="card-title mt-80">{name}</h2>
+                <p>{login}</p>
               </div>
             </div>
-            <div className="col-span-2">
-              <div className="mb-6">
-                <h1 className="text-3xl card-title">
-                  {name}
-                  <div className="ml-2 mr-1 badge badge-success text-white">
-                    {type}
-                  </div>
-                  <div>
-                    {hireable && (
-                      <div className="mx-1 badge badge-info text-white">
-                        Hireable{' '}
-                      </div>
-                    )}
-                  </div>
-                </h1>
-                <p>{bio}</p>
-                <div className="mt-4 card-actions">
-                  <a
-                    href={html_url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="btn"
-                  >
-                    {' '}
-                    Visit Github Profile
-                  </a>
+          </div>
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                {name}
+                <div className="ml-2 mr-1 badge badge-success text-white">
+                  {type}
                 </div>
+                <div>
+                  {hireable && (
+                    <div className="mx-1 badge badge-info text-white">
+                      Hireable{' '}
+                    </div>
+                  )}
+                </div>
+              </h1>
+              <p>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a
+                  href={html_url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn"
+                >
+                  {' '}
+                  Visit Github Profile
+                </a>
               </div>
-              <div className="w-full rounded-lg shadow-md bg-base-100 stats">
-                {location && (
-                  <>
-                    <div className="stat-title text-md"> Location</div>
-                    <div className="text-lg stat-value"> {location}</div>
-                  </>
-                )}
+            </div>
+            <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+              {location && (
+                <div className="stat">
+                  <div className="stat-title text-md"> Location</div>
+                  <div className="text-lg stat-value"> {location}</div>
+                </div>
+              )}
 
-                {blog && (
-                  <>
-                    <div className="mt-2 stat-title text-md"> Website</div>
-                    <div className="text-lg stat-value">
+              {blog && (
+                <div className="stat">
+                  <div className="mt-2 stat-title text-md"> Website</div>
+                  <div className="text-lg stat-value">
+                    {' '}
+                    <a
+                      href={`https://${blog}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
                       {' '}
-                      <a
-                        href={`https://${blog}`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        {' '}
-                        {blog}{' '}
-                      </a>
-                    </div>
-                  </>
-                )}
+                      {blog}{' '}
+                    </a>
+                  </div>
+                </div>
+              )}
 
-
-{twitter_username && (
-                  <>
-                    <div className="mt-2 stat-title text-md"> Twitter</div>
-                    <div className="text-lg stat-value">
+              {twitter_username && (
+                <div className="stat">
+                  <div className="mt-2 stat-title text-md"> Twitter</div>
+                  <div className="text-lg stat-value">
+                    {' '}
+                    <a
+                      href={`https://twitter.com/${twitter_username}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
                       {' '}
-                      <a
-                        href={`https://twitter.com/${twitter_username}`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        {' '}
-                        {blog}{' '}
-                      </a>
-                    </div>
-                  </>
-                )}
+                      {blog}{' '}
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            {/* try */}
+
+            <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <FaUserFriends
+                    className="text-3xl md:text-3xl"
+                  />
+                  </div>
+                  <div className="stat-title pr-5"> Following</div>
+                  <div className="stat-value pr-5 text-3xl md:text-3xl">
+                    {' '}
+                    {following}
+                  </div>
+                   </div>
+
+                   <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <FaUsers
+                    className="text-3xl md:text-3xl"
+                  />
+                  </div>
+                  <div className="stat-title pr-5"> Followers</div>
+                  <div className="stat-value pr-5 text-3xl md:text-3xl">
+                    {' '}
+                    {followers}
+                  </div>
+                   </div>
+                
+                   <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <FaCode
+                    className="text-3xl md:text-3xl"
+                  />
+                  </div>
+                  <div className="stat-title pr-5"> Pulic Repos</div>
+                  <div className="stat-value pr-5 text-3xl md:text-3xl">
+                    {' '}
+                    {public_repos}
+                  </div>
+                   </div>
+
+                   
+                   <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <FaStore
+                    className="text-3xl md:text-3xl"
+                  />
+                  </div>
+                  <div className="stat-title pr-5"> Pulic Repos</div>
+                  <div className="stat-value pr-5 text-3xl md:text-3xl">
+                    {' '}
+                    {public_gists}
+                  </div>
+                   </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      
     </>
   )
 }
