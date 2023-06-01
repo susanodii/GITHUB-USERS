5fba7e42305e9739ab53b529c247f28930731e07
susan <odiisusan@gmail.com>
2023-06-01 14:50:04 +0100

    replaced the fetch api with axios

--- src/Components/Context/Github/GithubContext.js
+++ src/Components/Context/Github/GithubContext.js
@@ -1,6 +1,7 @@
 import GithubReducer from './GithubReducer'
 import { createContext } from 'react'
 import { useReducer } from 'react'
+import axios from 'axios'
 
 // import {useState} from "react";
 
@@ -8,6 +9,11 @@ const GithubContext = createContext()
 const GITHUB_BASE_URL = process.env.REACT_APP_GITHUB_BASE_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
+
+const github= axios.create({
+  baseURL:GITHUB_BASE_URL,
+  headers: {Authorization: `token ${GITHUB_TOKEN}`},
+})
 export const GithubProvider = ({ children }) => {
   //     const [users, setUsers] = useState([])
   //    const [isLoading, setIsLoading] = useState(true)
@@ -38,43 +44,71 @@ export const GithubProvider = ({ children }) => {
     setIsLoading()
 
     const params = new URLSearchParams({ q: text })
-    const res = await fetch(`${GITHUB_BASE_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-        'Content-Type': `application/json`,
-      },
-    })
 
-    const { items } = await res.json()
-
+    const res=await github.get(`/search/users?${params}`)
+    const items= res.data.items
     dispatch({
-      type: 'GET_USERS',
-      payload: items,
+      type:'GET_USERS',
+      payload:items
     })
-  }
+
+    // using fetch api to get data from the end point
+  //   const res = await fetch(`${GITHUB_BASE_URL}/search/users?${params}`, {
+  //     headers: {
+  //       Authorization: `token ${GITHUB_TOKEN}`,
+        
+  //     },
+  //   })
+
+  //   const { items } = await res.json()
+
+  //   dispatch({
+  //     type: 'GET_USERS',
+  //     payload: items,
+  //   })
+   }
 
   
   // function to get a single user
   const getUser= async(login)=>{
     setIsLoading()
-    const res = await fetch(`${GITHUB_BASE_URL}/users/${login}`, {
+// using axios
 
-      headers: {
-        Authorization:`token ${GITHUB_TOKEN}`,
-         'Content-Type': `application/json`,
-        }
-    })
 
+
+    const res = await github.get(`/users/${login}`)
+
+    
     if(res.status ===404){
       window.location ='/notfound'
     }else{
-      const data = await res.json()
+      const data =  res.data
       
       dispatch({
         type: 'GET_USER',
       payload: data,
      })
     }
+
+// using fetch api to get data from the end point
+    // const res = await fetch(`${GITHUB_BASE_URL}/users/${login}`, {
+
+    //   headers: {
+    //     Authorization:`token ${GITHUB_TOKEN}`,
+    //      'Content-Type': `application/json`,
+    //     }
+    // })
+
+    // if(res.status ===404){
+    //   window.location ='/notfound'
+    // }else{
+    //   const data = await res.json()
+      
+    //   dispatch({
+    //     type: 'GET_USER',
+    //   payload: data,
+    //  })
+    // }
 }
 
 // function to get user repositories
@@ -86,19 +120,28 @@ const getUserRepos = async(login)=>{
     per_page:5,
 })
 
-const res= await fetch(`${GITHUB_BASE_URL}/users/${login}/repos?${params}`, {
-  headers:{
-    Authorization:`token ${GITHUB_TOKEN}`,
-         'Content-Type': `application/json`,
-  },
-})
+const res = await github.get(`/users/${login}/repos?${params}`)
 
-const data = await res.json()
+const data =  res.data
 
 dispatch({
   type:'GET_REPOS',
   payload:data,
 })
+// using fetch api to get data from the end point
+// const res= await fetch(`${GITHUB_BASE_URL}/users/${login}/repos?${params}`, {
+//   headers:{
+//     Authorization:`token ${GITHUB_TOKEN}`,
+//          'Content-Type': `application/json`,
+//   },
+// })
+
+// const data = await res.json()
+
+// dispatch({
+//   type:'GET_REPOS',
+//   payload:data,
+// })
 }
   return (
     <GithubContext.Provider
