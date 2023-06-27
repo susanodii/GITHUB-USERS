bba66921200399a1eb27bf7d99581e412bdc5a46
susan <odiisusan@gmail.com>
2023-06-27 21:57:15 +0100

    updated enviroment variable

--- src/Components/Context/Alert/AlertReducer.js
+++ src/Components/Context/Alert/AlertReducer.js
@@ -1,8 +1,8 @@
         
         const alertReducer =(state, action) =>{
              switch(action.type){
-                case 'SET_ALERT':
-                    return action.payload
+                case 'SET_ALERT' :
+                           return action.payload
                     case 'REMOVE_ALERT':
                         return null
                         default:
